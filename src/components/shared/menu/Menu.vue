c471d3111bc130791edcfa2f20e476abca93e9a4
João Gabriel Pancheski <joao.ti@grupointegrado.br>
2019-09-30 14:08:13 -0300

    Aula 06 - Criação de rotas

---
+++ src/components/shared/menu/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="route in routes" v-bind:key="route.titulo">
+        <router-link :to="route.path ? route.path : '/'">
+          {{ route.titulo }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    routes: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
