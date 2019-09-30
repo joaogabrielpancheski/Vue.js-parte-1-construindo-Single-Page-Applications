aebdae903d0494d349c93c228381d52d9dd8c7ad
João Gabriel Pancheski <joao.ti@grupointegrado.br>
2019-09-30 10:35:17 -0300

    Aula 04 - Criando componentes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos" v-bind:key="foto.titulo">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" v-bind:key="foto.titulo">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue'
+
 export default {
+  components: {
+    'meu-painel': Painel
+  },
+
   data() {
     return {
       titulo: 'Alurapic',
@@ -28,4 +39,25 @@ export default {
 
 <style>
 
+  .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
 </style>
